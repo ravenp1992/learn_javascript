0f9b217ddd49f2313ea9114c7cf1fd9b98e75505
Raven Paragas <ravenparagas@Ravens-MacBook-Pro.local>
2023-12-03 08:24:56 +0800

    Code structure

---
+++ javascript_fundamentals/02_code_structure/index.js
@@ -0,0 +1,10 @@
+// STATEMENTS
+// Statements are syntax construct and commands that perform actions.
+
+// SEMICOLONS
+// A semicolon may be omitted in most cases when a line break exists.
+
+// COMMENTS
+// One-line comments start with two forward slashes
+// Multime comments start with a forward slash and an asterisk
+/* */
