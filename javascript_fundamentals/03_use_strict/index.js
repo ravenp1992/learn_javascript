e32b6b3ecf00b683d90ed4c7409b19a3879fddea
Raven Paragas <ravenparagas@Ravens-MacBook-Pro.local>
2023-12-03 08:25:16 +0800

    Use strict mode

---
+++ javascript_fundamentals/03_use_strict/index.js
@@ -0,0 +1,24 @@
+// The modern mode, "use strict"
+
+// For a long time, JavaScript evolved without compatibility issues. New features
+// were added to the language while old functionality didn't change.
+
+// That had the benefit of never breaking existing code. But the downside was
+// that any mistakes or an impact decision made by JavaScript's creator got
+// stuck in the language forever.
+
+// This was the case until 2009 when ECMAScript 5 (ES5) appeared. It added new
+// features to the language and modified some of the existing ones. To keep the
+// old code working, most such modifications are off by default. You need to
+// explicitly enable them with a special directive: "use strict"
+
+// Ensure that "use strict" is at the top
+// Once we enter strict mode, there's no going back.
+
+// Should we "use stict"?
+// Modern JavaScript supports "classes" and "modules" - advanced language structures
+// that enable use strict automatically. So we don't need to add the "use strict"
+// directive, if we use them.
+
+// So, for now "use strict"; is a welcome guest at the top of your script. Later,
+// when your code is all in classes and modules, you may omit it.
